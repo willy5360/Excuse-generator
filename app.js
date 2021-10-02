6fc7502faa2fc77ca094f92205d80218dfc8b84c
willy5360 <84270420+willy5360@users.noreply.github.com>
2021-10-02 17:35:26 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,38 @@
+/* eslint-disable */
+
+import "./style.css";
+
+window.onload = () => {
+  //write your code here
+  document.querySelector("#excuse").innerHTML = generateExcuse();
+
+  console.log("Hello!");
+  console.log("Hello Rigo from the console!");
+};
+
+let generateExcuse = () => {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let whoIndx = Math.floor(Math.random() * who.length);
+  let actionIndx = Math.floor(Math.random() * action.length);
+  let whatIndx = Math.floor(Math.random() * what.length);
+  let whenIndx = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whoIndx] +
+    " " +
+    action[actionIndx] +
+    " " +
+    what[whatIndx] +
+    " " +
+    when[whenIndx]
+  );
+};
